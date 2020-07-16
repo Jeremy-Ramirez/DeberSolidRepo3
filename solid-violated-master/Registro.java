b28600e175902f41ee4fe8f5ccff9cd4bd34928d
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2020-07-16 17:55:25 -0500

    Corrección principio SRP

---
+++ solid-violated-master/Registro.java
@@ -0,0 +1,44 @@
+import control.Usuario;
+
+public class Registro {
+
+	
+	public static void registrarUsuario(Usuario u) {
+		
+
+		
+		if(u.firstName.length() == 0){
+	        System.out.println("First name cannot be empty");
+	        
+	    }
+
+	    if(u.lastName.length() == 0){
+	         System.out.println("Last name cannot be empty");
+	      
+	     }
+
+	     if(!u.emailAddress.matches("^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$")){
+	         System.out.println("Invalid email address format");
+	         
+	     }
+	     
+	     else {
+
+		      System.out.println("User Created Successfully");
+
+		      System.out.println("Sending Email to User");
+
+		      System.out.println("To: "+ u.emailAddress);
+		      System.out.println("Subject: Welcome Email to User");
+		      System.out.println("Body:");
+		      System.out.format("Hi %s %s,",u.firstName, u.lastName);
+		      System.out.println("Welcome to CodHer! \nWe are happy to have you as a user :-) ");
+	     }
+	     
+	     
+		
+	}
+	
+	
+	
+}
